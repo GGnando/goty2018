fbe474b8baad508e1f2645cb707e61a2af23c9a7
Styerstyler8 <styerstyler8@gmail.com>
2018-03-01 16:23:45 -0500

    Fixing Testing
    This is a pain

---
+++ memes/Assets/Scripts/InventoryScripts/Inventory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    //This line and awake ensures 1 inventory at time and shares with all instances of class. Always able to access this inventory easy
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than one instance of inventory.");
+            return;
+        }
+
+        instance = this;
+    }
+
+    //Used for event handeling and helping with UI of inventory
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    //Creates list to hold items
+    public List<Item> items = new List<Item>();
+
+    //Getters for List of items for testing
+    public List<Item> getItems()
+    {
+        return items;
+    }
+
+    //Adds item to list
+    public void Add(Item item)
+    {
+        items.Add(item);
+
+        if (onItemChangedCallback != null)
+        {
+            //Triggering event to update UI
+            onItemChangedCallback.Invoke();
+        }
+
+    }
+
+    //Removes item from list
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallback != null)
+        {
+            //Triggering event to update UI
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
