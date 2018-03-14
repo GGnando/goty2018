0a015d6ce85d14e8577517353617f3c78ad4a35f
Styerstyler8 <styerstyler8@gmail.com>
2018-03-14 00:41:13 -0400

    Master updated
    Master now has Tyler's, Raj's, Larry's, Denny's, and Fernando's current branches. Sorry Ryan...

---
+++ memes/Assets/Scripts/InventoryScripts/Inventory.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    //This line and awake ensures 1 inventory at time and shares with all instances of class. Always able to access this inventory easy
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than one instance of inventory.");
+            return;
+        }
+
+        instance = this;
+    }
+
+    //Used for event handeling and helping with UI of inventory
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    //Creates list to hold items of each type
+    public List<Item> items = new List<Item>();
+
+    //List of all the types of items
+    public List<Item> weapons = new List<Item>();
+    public List<Item> shields = new List<Item>();
+    public List<Item> armor = new List<Item>();
+    public List<Item> consumables = new List<Item>();
+    public List<Item> resources = new List<Item>();
+    public List<Item> quests = new List<Item>();
+
+    //Getters for List of items for testing
+    public List<Item> getItems()
+    {
+        return items;
+    }
+
+    //Adds item to list
+    public void Add(Item item)
+    {
+        items.Add(item);
+
+        //Add specific item to item type list:
+        if(item.itemType == Item.ItemType.Quest)
+        {
+            quests.Add(item);
+        }
+        if(item.itemType == Item.ItemType.Resource)
+        {
+            resources.Add(item);
+        }
+        if(item.itemType == Item.ItemType.Consumable)
+        {
+            consumables.Add(item);
+        }
+        if (item.itemType == Item.ItemType.Armor)
+        {
+            armor.Add(item);
+        }
+        if (item.itemType == Item.ItemType.Shield)
+        {
+            shields.Add(item);
+        }
+        if (item.itemType == Item.ItemType.Weapon)
+        {
+            weapons.Add(item);
+        }
+
+        if (onItemChangedCallback != null)
+        {
+            //Triggering event to update UI
+            onItemChangedCallback.Invoke();
+        }
+
+    }
+
+    //Removes item from list
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        //Remove item from its list:
+        if (item.itemType == Item.ItemType.Quest)
+        {
+            quests.Remove(item);
+        }
+        if (item.itemType == Item.ItemType.Resource)
+        {
+            resources.Remove(item);
+        }
+        if (item.itemType == Item.ItemType.Consumable)
+        {
+            consumables.Remove(item);
+        }
+        if (item.itemType == Item.ItemType.Armor)
+        {
+            armor.Remove(item);
+        }
+        if (item.itemType == Item.ItemType.Shield)
+        {
+            shields.Remove(item);
+        }
+        if (item.itemType == Item.ItemType.Weapon)
+        {
+            weapons.Remove(item);
+        }
+
+        if (onItemChangedCallback != null)
+        {
+            //Triggering event to update UI
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
---
+++ memes/Assets/Scripts/InventoryScripts/InventoryUI.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+//This class keeps track of inventory and updates it
+public class InventoryUI : MonoBehaviour {
+
+    //Reference to item slot's parent. Used to loop through and pick item slot
+    public Transform itemsParent;
+
+    //Reference to entire inventory
+    public GameObject inventoryUI;
+
+    //Array to keep track and use inventory slots 
+    InventorySlot[] itemSlots;
+
+    //Stores inventory in variable
+    Inventory inventory;
+
+    [SerializeField] GameObject itemSlotPrefab;
+
+    void Start () {
+        //Set inventory and update UI
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += updateUI;
+
+        //This calls the parent to get all slots and add to array
+        itemSlots = itemsParent.GetComponentsInChildren<InventorySlot>();
+	}
+	
+	void Update () {
+        //To pull up and get off of inventory, look for inventory key press
+        if (Input.GetButtonDown("Inventory"))
+        {
+            //Does opposite of whatever is up, so if inventory is pulled up, pressing again closes it
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+	}
+
+    void updateUI()
+    {
+        Debug.Log("Updating UI");
+
+        //Go through all slots to add item
+        for(int i = 0; i < itemSlots.Length; i++)
+        {
+            //Means inventory isnt full
+            if(i < inventory.items.Count)
+            {
+                GameObject itemSlot = Instantiate(itemSlotPrefab);
+
+                //Add item to specific inventory slot
+                itemSlots[i].addItem(inventory.items[i]);
+            }
+            else
+            {
+                //Out of inventory items to add
+                itemSlots[i].clearSlot();
+            }
+        }
+
+    }
+}
