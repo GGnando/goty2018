7c491c9aafbd68427f16776897634762efcbd91b
Styerstyler8 <styerstyler8@gmail.com>
2018-03-18 08:10:54 -0400

    Inventory So Far
    This is what my inventory and items are looking like

--- Assets/Scripts/InventoryScripts/Inventory.cs
+++ Assets/Scripts/InventoryScripts/Inventory.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
 
     //This line and awake ensures 1 inventory at time and shares with all instances of class. Always able to access this inventory easy
     public static Inventory instance;
+
+    //Allows access to consuming items
+    public ConsumablesController consumableController;
+
+    //Reference to panel that displays item info
+    public InventoryUIDetails inventoryDetailsPanel;
 
     private void Awake()
     {
@@ -17,6 +24,18 @@ public class Inventory : MonoBehaviour {
 
         instance = this;
     }
+
+    //Testing inventory system here:
+    private void Start()
+    {
+        Item debugPotion = new Item("DebugPotion", Resources.Load<Sprite>("InventoryUI/icons/png/64px/Inventory_Potion"), "Testing potion", Item.ItemType.Consumable, true, true, 5, 0.5, 1, 1, "Drink", true);
+        Item healthPotion = new Item("Health Potion", Resources.Load<Sprite>("InventoryUI/icons/png/64px/Weapons_Sword"), "This is a health potion. What do you think it does.", Item.ItemType.Consumable, true, true, 10, 0.3, 1, 1, "Drink", true);
+        Add(debugPotion);
+        Add(healthPotion);
+        Debug.Log("Added some potions (Delete this later)");
+
+    }
+
 
     //Used for event handeling and helping with UI of inventory
     public delegate void OnItemChanged();
@@ -70,6 +89,9 @@ public class Inventory : MonoBehaviour {
             weapons.Add(item);
         }
 
+        //Adds to event and calls everything subscribed to event, so all inventory gets updated and displayed
+        UIEventHandler.ItemAddedToInventory(item);
+
         if (onItemChangedCallback != null)
         {
             //Triggering event to update UI
@@ -114,5 +136,20 @@ public class Inventory : MonoBehaviour {
             //Triggering event to update UI
             onItemChangedCallback.Invoke();
         }
+    }
+
+    //Method for displaying item info
+    public void setItemDetails(Item item, Button button)
+    {
+        inventoryDetailsPanel.setItem(item, button);
+    }
+
+    //Equiping item from inventory code:
+    //Add equip weapons later
+
+    //Consumables:
+    public void ConsumeItem(Item consumedItem)
+    {
+        consumableController.consumeItem(consumedItem);
     }
 }
