fecaa748c33b488807236e2b427f0b239b843211
Dennys <dennys1234pelegrin@gmail.com>
2018-03-27 21:47:24 -0400

    Why

---
+++ Assets/Scripts/DialogueAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueAudio : MonoBehaviour {
+    public AudioClip pushButton;
+    public AudioSource buttonSource;
+
+	// Use this for initialization
+	void Start () {
+        buttonSource.clip = pushButton;
+	}
+
+    // Update is called once per frame
+    public void buttonNoise()
+    {
+        buttonSource.Play();
+    }
+}
---
+++ Assets/Scripts/InventoryScripts/InventoryUIDetails.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUIDetails : MonoBehaviour {
+
+    Item item; //Item selected
+    Button selectedItemButton; //Button to item selected
+    Button itemInteractButton; //Button to interact with item
+    Text itemName; //Name of item to appear in details panel
+    Text itemDescriptionText; //Description of item to appear
+    Text itemButtonText; //Button text when interacting with it
+
+    Text statText; //Text of stats of item
+
+    void Start()
+    {
+        //Setting all the information for the panel
+        itemName = transform.Find("ItemName").GetComponent<Text>();
+        itemDescriptionText = transform.Find("ItemDescription").GetComponent<Text>();
+        itemInteractButton = transform.Find("Button").GetComponent<Button>();
+        itemButtonText = itemInteractButton.transform.Find("Text").GetComponent<Text>();
+
+        //When no item selected, just deactivated
+        gameObject.SetActive(false);
+    }
+    //Set item to display info on side of items
+    public void setItem(Item item, Button button)
+    {
+        //Show details when object selected
+        gameObject.SetActive(true);
+
+        //statText.text = "";
+
+        //If item does not have stats, don't display. If so, loop through and display all
+        /*
+        if(item.stats != null)
+        {
+            foreach(Stat stat in item.stats)
+            {
+                statText.text += stat.StatName + ": " + stat.Value + "\n";
+            }
+        }
+        */
+
+        //Remove previous listeners for events or everything used will be done multiple times:
+        itemInteractButton.onClick.RemoveAllListeners();
+
+        //Setting information for details pannel to show
+        this.item = item;
+        selectedItemButton = button;
+        itemName.text = item.name;
+        itemDescriptionText.text = item.itemDescription;
+        itemButtonText.text = item.interactName;
+
+        //When button clicked to interact with item, call below method
+        itemInteractButton.onClick.AddListener(onItemInteract); 
+    }
+
+    //Actually use item time
+    public void onItemInteract()
+    {
+        //Depending on type of item, interact in other ways
+        if(item.itemType == Item.ItemType.Consumable)
+        {
+            Inventory.instance.ConsumeItem(item);
+            Destroy(selectedItemButton.gameObject);
+        }
+        //else if for weapon here
+
+        //Null item after using and hide item details when not on something
+        item = null;
+        gameObject.SetActive(false);
+        
+    }
+}
