857c569187067742b8ba7457e61b421e089737fe
Styerstyler8 <styerstyler8@gmail.com>
2018-04-02 21:02:22 -0400

    Master has been updated
    Master now has a single scene containing: Larry's, Dennys', Fernando's, and Ryan's Scenes. Working on others.

---
+++ Assets/Scripts/QuestScripts/Objective.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Base class for quest goals/objectives. What the player has to do
+public class Objective  {
+    public Quest quest; //From quest class
+    public string description; //Description of what the objective is
+    public bool completed; //False if quest objective is not done yet
+    public int currProgress; //int of current progress. Ex. kill 5 enemies curr would be 0 at start
+    public int endAmount; //int of end amount needed to finish objective
+
+    //Initialization method for objective
+    public virtual void initialization()
+    {
+        //Base functionality if needed here
+        //Extended by other types of objectives for different initilization needs
+    }
+
+    //Checks progress of objective. Calls completed when required amount is reached
+    public void progress()
+    {
+        if(currProgress >= endAmount)
+        {
+            completion();
+        }
+    }
+
+    //Quest is completed
+    public void completion()
+    {
+        completed = true;
+    }
+}
---
+++ Assets/Scripts/QuestScripts/Quest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq; //library to help with quest objectives
+
+public class Quest : MonoBehaviour {
+
+    //Basis for quest will have objectives, a name, description, XP reward, item reward, and bool to see if complete
+    public List<Objective> objectives = new List<Objective>();
+    public string questName;
+    public string questDescription;
+    public int experienceAward;
+    public string itemReward;
+    public bool isComplete;
+
+    public GameObject player; //Reference to player to give xp
+
+    public void checkObjectives()
+    {
+        isComplete = objectives.All(g => g.completed); //Goes through objectives to see if one objective is not completed
+
+        //Give reward if quest is complete
+        if (isComplete)
+        {
+            giveReward();
+        }
+    }
+
+    void giveReward()
+    {
+        Debug.Log("Quest was completed");
+        if (itemReward != null)
+        {
+            //If item reward exsits, add to inventory once completed
+            Inventory.instance.Add(itemReward);
+        }
+
+        if (experienceAward != 0)
+        {
+            //If experience award exsits, give xp to player
+            player.GetComponent<Experience>().AddXP(experienceAward);
+        }
+    }
+}
