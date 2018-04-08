34b1c3ebc5211a341d2c92b2253d826c38ac3581
Dennys <dennys1234pelegrin@gmail.com>
2018-04-08 15:28:59 -0400

    Doin it again

---
+++ Assets/GoblinSounds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinSounds : MonoBehaviour {
+  
+    public void GoblinLaugh()
+    {
+        FindObjectOfType<DialogueAudio>().GoblinLaugh();
+    }
+
+    public void GoblinSwing()
+    {
+        FindObjectOfType<DialogueAudio>().GoblinSwing();
+    }
+}
