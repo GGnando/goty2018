c7809f9f6540f1b9e68dacf59f321d04fc029fbc
Styerstyler8 <styerstyler8@gmail.com>
2018-04-22 06:28:47 -0400

    Merged Raj stuff
    This includes different types of weapons, crafting items, and items. Also inclusion of health potion

---
+++ Assets/Scripts/ItemScripts/Consumables/HealthPotion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is an example of creating a script for a consumable
+public class HealthPotion : MonoBehaviour, IConsumables
+{
+    GameObject player;
+
+    //2 different possibilities for consuming item. One that just does something, and one that effects character's stats
+    //This allows each item to have a certain consume function
+    public void consume()
+    {
+        //Increase health of player by 40
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<Health>().Heal(40);
+    }
+
+    public void consume(CharacterStat stats)
+    {
+    }
+}
